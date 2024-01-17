526b0942fc9441c084feb465b91d27cda04f0c27
João Pedro Alexandrino <sonderinterprise@gmail.com>
2024-01-17 20:00:15 -0300

    Numeric functions Exemple

--- teste.py
+++ teste.py
@@ -2,4 +2,24 @@ numero_int = 4
 
 numero_int2 = 6
 
-print("A multiplicação é =", numero_int * numero_int2)
+print("classe é =",type(numero_int2))
+
+# Soma
+soma = 100 + 1
+print("Soma =", soma)
+
+# Subração
+subtracao = 100 - 1
+print("Subtração =", subtracao)
+
+# Multiplicação
+multiplcacao = 100 * 2
+print("Multiplcação =", multiplcacao)
+
+# Divisão // Divisão int pode ser feira por "//" ao inves de apenas "/"
+divisao = 100 / 2
+print("Divisão =", divisao)
+
+# Modulo = Resto da divisão 
+modulo = 5 % 2
+print("Modulo", modulo)
