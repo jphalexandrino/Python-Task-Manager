9b65e580869d189c41d5e82648da518ae3525c21
João Pedro Alexandrino <sonderinterprise@gmail.com>
2024-01-18 20:00:40 -0300

    Created the `program interface`

---
+++ manager.py
@@ -0,0 +1,16 @@
+# Criação do menu
+while True:
+    print("\n Task Manager Menu:")
+    print("1. Add task")
+    print("2. View task")
+    print("3. Update task")
+    print("4. Complete task")
+    print("5. Delete completed task")
+    print("6. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "6":
+        break
+
+print("Completed Program")
