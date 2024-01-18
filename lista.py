5cc9e03659e9aa9d8a50f685ccb3789bff68ac24
João Pedro Alexandrino <sonderinterprise@gmail.com>
2024-01-18 00:05:07 -0300

    Add `List Exemples`

---
+++ lista.py
@@ -0,0 +1,29 @@
+# Declaration 
+
+my_lisy = [1, 2, 3, 4, 5, "Avocato", True, False]
+
+
+print("My list contains:", my_lisy)
+#List Exemples
+# 1º iten contains
+print("My list[0] contains:", my_lisy[0])
+# 2º iten contains
+print("My list[1] contains:", my_lisy[1])
+# 3º iten contains
+print("My list[2] contains:", my_lisy[2])
+# 4º iten contains
+print("My list[3] contains:", my_lisy[3])
+# 5º iten contains
+print("My list[4] contains:", my_lisy[4])
+# 6º iten contains
+print("My list[5] contains:", my_lisy[5])
+# 7º iten contains
+print("My list[6] contains:", my_lisy[6])
+# 8º iten contains
+print("My list[7] contains:", my_lisy[7])
+# Slice
+print("Slice [1:7]:", my_lisy[1:7])
+# Slice to
+print("Slice to element 6:", my_lisy[:6])
+# Slice afther
+print("Slice to element 6:", my_lisy[2:])
