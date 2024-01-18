7b708d6402b71c9259546ace03945525228fb73a
João Pedro Alexandrino <sonderinterprise@gmail.com>
2024-01-18 01:43:00 -0300

    add dictionary type exemple

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+pessoa = {
+    "name": "João",
+    "surname": "Alexandrino",
+    "age": 16,
+    "city": "Novo Hamburgo"
+}
+
+print("name:", pessoa["name"])
+print("surname:", pessoa["surname"])
+print("age:", pessoa["age"])
+print("city:", pessoa["city"])
