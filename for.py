8555bf607500dd9f3fe6df4f0ccfff2a39971dc1
João Pedro Alexandrino <sonderinterprise@gmail.com>
2024-01-18 19:51:02 -0300

    Finish the examples - Start the project

--- for.py
+++ for.py
@@ -1,9 +1,11 @@
+"""
 print("For using list")
 list = [1, 2, 3, 4, 5]
 for element in list:
     print(element)
+"""
 
-print("\nFor using tuple")
+print("\n For using tuple")
 tuple = (1, 2, 3, 4, 5)
 for element in tuple:
     print(element)
@@ -13,14 +15,26 @@ user = {
     "age": 16,
     "city": "Novo Hamburgo"
 }
-print("\nFor using dictionary - keys")
+print("\n For using dictionary - keys")
 for keys in user.keys():
     print(keys)
 
-print("\nFor using dictionary - Values")
+print("\n For using dictionary - Values")
 for value in user.values():
     print(value)
 
-print("\nFor using dictionary - Items")
+print("\n For using dictionary - Items")
 for keys, value in user.items():
     print(f"{keys}: {value}")
+
+# range():
+    print("\n Using Range() function")
+for number in range(5):
+    print("Number:", number)
+
+# Len()
+print("\n Using Range() function with len()")
+list2 = [1, 2, 3, 4, 5]
+for index in range(0, len(list2)):
+    print("Index:", index)
+    print("Element:", list2[index])
---
+++ while.py
@@ -0,0 +1,8 @@
+print("Exemple of While")
+conter = 0
+while conter < 70:
+    print("Score:", conter)
+    conter += 1
+    #if conter == 50:
+    #    break
+print("Count finished")
